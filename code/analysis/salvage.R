45509a8c6c61b50c2d854805b729378343838b11
rosehartman <rosehartman@gmail.com>
2025-12-08 14:34:20 -0800

    Predicting sediment based on turbidity

--- code/analysis/salvage.R
+++ code/analysis/salvage.R
@@ -5,6 +5,7 @@
 #covered species: Longfin, Chinook, Delta smelt, Green Sturgeon, White sturgeon, steelhead(?)
 
 library(tidyverse)
+library(effects)
 library(deltafish)
 
   ######pull Salsvage data ############
@@ -48,8 +49,18 @@ load("data/processed/storms/StormData.RData")
 
 Sacflow_wstorms = mutate(Sacflow_wstorms, Volume = YoloSac/43560*86400)
 
+#add exports
+load("data/external/Dayflow.RData")
+
+Exports = select(Dayflow, Date, Year, CVP, SWP) %>%
+  mutate(Exports = CVP+SWP)
+
+Sacflow_wstorms = Sacflow_wstorms %>%
+  left_join(Exports)
+
+
 Storms = group_by(Sacflow_wstorms, ID) %>%
-  summarize(TotalFlow = sum(Volume))
+  summarize(TotalFlow = sum(Volume), Exports = sum(Exports, na.rm =T))
 
 FirstStorms = left_join(Firststorms, Storms)
 
@@ -63,8 +74,10 @@ annual_salvage = df_salvage %>%
          CPUE = Count/Tow_volume) %>%
   group_by(WY, Taxa) %>%
   summarize(CPUE = sum(CPUE, na.rm =T), Count = sum(Count)) %>%
-  left_join(select(FirstStorms, Date, WY, SAC, YOLO, TotalFlow)) %>%
-mutate(Month = month(Date), DOWY = case_when(Month %in% c(10,11,12) ~ yday(Date)-275, TRUE ~  yday(Date)+91))
+  left_join(select(FirstStorms, Date, WY, SAC, YOLO, TotalFlow, Exports)) %>%
+mutate(Month = month(Date), DOWY = case_when(Month %in% c(10,11,12) ~ yday(Date)-275, TRUE ~  yday(Date)+91),
+       regime = case_when(WY < 2008 ~ "pre-2008",
+                          WY >= 2008 ~ "post-2008"))
 
 
 #timing of first flush versus annualsalvage
@@ -75,7 +88,7 @@ ggplot(annual_salvage, aes(x = DOWY, y = log(CPUE)))+ geom_point() + geom_smooth
 
 ggplot(annual_salvage, aes(x = DOWY, y = log(Count)))+ geom_point() + geom_smooth(method = "lm")+
   facet_wrap(~Taxa, scales = "free_y")+
-  xlab("Day of water year of first flush")
+  xlab("Day of water year of first flush") +ylab("Log of annual salvage")
 
 #fish seen in predator flush that don't count toward salvage.
 
@@ -89,9 +102,190 @@ ggplot(annual_salvage, aes(x = log(YOLO+SAC), y = log(Count)))+ geom_point() + g
 #strenth of first flush versus annualsalvage
 ggplot(annual_salvage, aes(x = log(TotalFlow), y = log(Count)))+ geom_point() +
   geom_smooth(method = "lm")+
-  facet_wrap(~Taxa, scales = "free_y")+ xlab("log-transformed Strength of first flush (total AF)")
+  facet_wrap(~Taxa, scales = "free_y")+ xlab("log-transformed Strength of first flush (total AF)")+
+  ylab("total annual salvage (log-transformed)")
+
+#Exports versus annual salvage
+
+ggplot(annual_salvage, aes(x = log(Exports), y = log(Count)))+ geom_point() +
+  geom_smooth(method = "lm")+
+  facet_wrap(~Taxa, scales = "free_y")+ xlab("log-transformed exports during first flush")
+
+#Salvage versus expot/inflow ratio
+
+ggplot(annual_salvage, aes(x = Exports/TotalFlow, y = log(Count)))+ geom_point() +
+  geom_smooth(method = "lm")+
+  facet_wrap(~Taxa, scales = "free_y")+ xlab("percentage of inflow exported")
 
 #look at effect of exports on this relationsihp. BiOps try and limit entrainment.
 
 #magnitude of exports during first flush driving seasonal salvage?
  #Look at % of first flush exported?
+
+#OK, now see if there is a significant relationship if i seperate pre-2008 from post-2008
+annual_salvage = mutate(annual_salvage, regime = case_when(WY < 2008 ~ "pre-2008",
+                                                           WY >= 2008 ~ "post-2008"))
+
+
+ggplot(annual_salvage, aes(x = log(TotalFlow), y = log(Count)))+ geom_point() +
+  geom_smooth(method = "lm")+
+  facet_grid(Taxa ~ regime, scales = "free_y")+ xlab("log-transformed Strength of first flush (total AF)")+
+  ylab("total annual salvage (log-transformed)")
+
+ggplot(annual_salvage, aes(x = log(Exports), y = log(Count)))+ geom_point() +
+  geom_smooth(method = "lm")+
+  facet_grid(Taxa ~ regime, scales = "free_y")+ xlab("log-transformed exports during first flush (total AF)")+
+  ylab("total annual salvage (log-transformed)")
+
+#now some statistics
+
+smelt = filter(annual_salvage, Taxa == "Hypomesus transpacificus")
+longfin = filter(annual_salvage, Taxa == "Spirinchus thaleichthys" )
+
+lm1 = lm(log(Count)~ log(TotalFlow)+ WY, data = smelt)
+summary(lm1)
+#nope
+
+#timing
+lm1 = lm(log(Count)~DOWY+ WY, data = smelt)
+summary(lm1)
+#also nope
+
+#has there been a change in strength of first flush over tiem?
+
+ggplot(annual_salvage, aes(x = WY, y = TotalFlow)) + geom_point()+ geom_smooth()
+
+ggplot(FirstStorms, aes(x = WY, y = log(TotalFlow))) + geom_point()+ geom_smooth(method = "lm")
+
+
+ggplot(filter(FirstStorms, Exports !=0), aes(x = WY, y = Exports)) + geom_point()+ geom_smooth(method = "lm")
+
+#has ff been changing its timing?
+
+FirstStorms = mutate(FirstStorms, Month = month(Date), DOWY = case_when(Month %in% c(10,11,12) ~ yday(Date)-275, TRUE ~  yday(Date)+91))
+
+
+
+ggplot(FirstStorms, aes(x = WY, y = DOWY)) + geom_point()+ geom_smooth(method = "lm")
+
+
+#what was going on in 2006?
+
+ggplot(filter(Sacflow_wstorms, WY == 2006), aes(x = Date, y = YoloSac))+ geom_point()+ geom_line()
+
+
+ggplot(filter(Sacflow_wstorms, WY == 2017), aes(x = Date, y = YoloSac))+ geom_point()+ geom_line()
+
+
+ggplot(filter(Sacflow_wstorms, WY == 2022), aes(x = Date, y = YoloSac))+ geom_point()+ geom_line()
+
+ggplot(filter(Sacflow_wstorms, WY == 2017), aes(x = Date, y = YoloSac))+ geom_point()+ geom_line()
+
+#OK, a few other things to look at
+# number of storms versus total salvage
+#oh, can i add the FMWT index as a factor?
+ScientificNames = data.frame(CommonName = c("Threadfin Shad", "American Shad", "Delta Smelt",
+                                            "Longfin Smelt", "Striped Bass Age0", "Splittail"),
+                             Taxa = c("Dorosoma petenense", "Alosa sapidissima", "Hypomesus transpacificus",
+                                       "Spirinchus thaleichthys", "Morone saxatilis", "Pogonichthys macrolepidotus"))
+
+FMWT = read_csv("data/raw/FMWTindices.csv")
+
+FMWTsmelt =FMWT %>%
+  pivot_longer(cols = c(`Threadfin Shad`:last_col()), names_to = "CommonName", values_to = "FMWTIndex") %>%
+  rename(WY = Year) %>%
+  left_join(ScientificNames)
+
+annual_salvageb = left_join(annual_salvage, FMWTsmelt)
+
+
+smelt = filter(annual_salvageb, Taxa == "Hypomesus transpacificus")
+longfin = filter(annual_salvageb, Taxa == "Spirinchus thaleichthys" )
+
+lm2 = lm(log(Count)~ log(TotalFlow)+ log(FMWTIndex+1), data = smelt)
+summary(lm2)
+plot(allEffects(lm2))
+#nope
+
+#timing
+lm3 = lm(log(Count)~DOWY+ FMWTIndex, data = smelt)
+summary(lm3)
+#also nope
+
+#longfin?
+lm4 = lm(log(Count+1)~DOWY+ FMWTIndex, data = filter(longfin, !is.na(DOWY)))
+summary(lm4)
+#definitely no
+
+lm4 = lm(log(Count+1)~log(TotalFlow)+ log(FMWTIndex), data = filter(longfin, !is.na(DOWY)))
+summary(lm4)
+#oh, something's here! Higher FF means less entrainment.
+plot(allEffects(lm4))
+
+#OK, now total number of storms in the winter
+#Would it be number of storms? Number of storm days? or amount of storm flow?
+
+StormSummary = Sacflow_wstorms %>%
+  group_by(WY) %>%
+  summarize(StormDays = length(YSStorm[which(!is.na(YSStorm))]), StormVolume = sum(Volume[which(!is.na(YSStorm))]),
+            StormN = length(unique(ID[which(!is.na(ID))])), StormExports = sum(Exports[which(!is.na(YSStorm))]),
+            Exports = sum(Exports, na.rm =T))
+
+annual_salvagec = annual_salvageb %>%
+  left_join(StormSummary)
+
+smelt = filter(annual_salvagec, Taxa == "Hypomesus transpacificus")
+longfin = filter(annual_salvagec, Taxa == "Spirinchus thaleichthys" )
+
+ggplot(annual_salvagec, aes(x = log(StormVolume), y = log(Count+1))) + geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~Taxa, scales = "free_y")+
+  ylab("Salvage (log-transformed)")+ xlab("Total volume during storms for the whole water year")
+
+ggplot(annual_salvagec, aes(x = log(StormExports), y = log(Count+1))) + geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~Taxa, scales = "free_y")+
+  ylab("Salvage (log-transformed)")+ xlab("Total exports during storms for the whole water year")
+
+
+ggplot(annual_salvagec, aes(x = StormDays, y = log(Count+1))) + geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~Taxa, scales = "free_y")+ xlab("number of storm days")
+
+
+ggplot(annual_salvagec, aes(x = StormN, y = log(Count+1))) + geom_point()+
+  geom_smooth(method = "lm")+
+  facet_wrap(~Taxa, scales = "free_y")+ xlab("number of storm events")
+
+#ok, now a model
+lm5 = lm(log(Count+1)~StormVolume+StormExports+ log(FMWTIndex+1), data = filter(longfin, !is.na(DOWY)))
+summary(lm5)
+#beleh
+library(car)
+vif(lm5)
+#volume and exports are pretty correlated
+
+lm6 = lm(log(Count+1)~StormVolume+ log(FMWTIndex+1), data = filter(longfin, !is.na(DOWY)))
+summary(lm6)
+#beleh
+vif(lm6)
+plot(allEffects(lm6))
+#much better
+
+#what about delta smelt?
+
+lm6s = lm(log(Count+1)~StormVolume+WY+ log(FMWTIndex+1), data = filter(smelt, !is.na(DOWY)))
+summary(lm6s)
+#beleh
+vif(lm6s)
+#And WY is pretty autocorrelated with the FMWT index. Sigh.
+plot(allEffects(lm6s))
+
+ggplot(annual_salvagec, aes(x = StormVolume, y = StormExports)) + geom_smooth(method = "lm")+
+  geom_point()
+#nope
+ggplot(annual_salvagec, aes(x = log(StormVolume), y = log(TotalFlow))) + geom_smooth(method = "lm")+
+  geom_point()
+
+#check whether FF volume or all storm volumes are better predictors for longfin salvage
+#get abundance indices for sturgeon and salmon
--- longfin restoration graphs.R
+++ longfin restoration graphs.R
@@ -15,13 +15,15 @@ sitevisit  <- read_csv("https://pasta.lternet.edu/package/data/eml/edi/269/5/1c1
 #filter to just Tule Red, Ryer, Grizzly Bay
 
 TR = filter(sitevisit, Location %in% c("Grizzly Bay", "Tule Red", "Ryer Island" )) %>%
-  left_join(FRPfish)
+  left_join(FRPfish) %>%
+  mutate(CPUE = Count/effort) %>%
+  filter(GearTypeAbbreviation != "HOOK")
 
 #add in zeros
 
 TR_z = pivot_wider(TR, id_cols = c(VisitNo, Location, Date, Temp, SC, DO, Turbidity,
                                    Chlorophyll, LatitudeStart, LongitudeStart, effort),
-                   names_from = CommonName, values_from = Count, values_fill = 0, values_fn = sum)
+                   names_from = CommonName, values_from = CPUE, values_fill = 0, values_fn = sum)
 
 ggplot(TR_z, aes(x = Date, y = `Longfin Smelt`)) + geom_point()+ geom_line() +
   facet_wrap(~Location)+ geom_vline(xintercept = ymd("2019-10-15"), color = "red", linetype =2)+
@@ -32,13 +34,77 @@ ggplot(TR_z, aes(x = Date, y = `Longfin Smelt`)) + geom_point()+ geom_line() +
 TR_z2 = TR_z %>%
   mutate(Year = year(Date)) %>%
   group_by(Location, Year) %>%
-  summarize(Longfin = sum(`Longfin Smelt`, na.rm =T))
+  summarize(Longfin = mean(`Longfin Smelt`, na.rm =T))
 
+#longfin from 2024 which I got from the FRP report
 Longfin2024 = data.frame(Location = c("Tule Red", "Grizzly Bay", "Ryer Island"),
                          Longfin = c(36, 108, 6), Year = 2024)
 
-TR_z2z = bind_rows(TR_z2, Longfin2024)
+#longfin from the FRP data from 2024
+Longfin2024_frp = read_csv("data/raw/2024.fish.FRP.EDI_2025-08-21.csv") %>%
+  filter(year(Date) == 2024, Location %in% c("Tule Red", "Grizzly Bay", "Ryer Island"), GearTypeAbbreviation != "HOOK") %>%
+  mutate(CPUE = Count/effort, Year = year(Date)) %>%
+  pivot_wider(id_cols = c(VisitNo, Year,Location, Date,  LatitudeStart, LongitudeStart, effort),
+              names_from = CommonName, values_from = CPUE, values_fill = 0, values_fn = sum)%>%
+  group_by(Location, Year) %>%
+  summarize(Longfin = mean(`Longfin Smelt`, na.rm =T))
+
+#let's try catch per 10000 cubpic meters
+
+TR_z2z = bind_rows(TR_z2, Longfin2024_frp) %>%
+  mutate(CPUE2 = Longfin*10000, loglong = log(CPUE2+1))
 
 ggplot(TR_z2z, aes(x = Year, y = Longfin)) + geom_point()+ geom_line() +
   facet_wrap(~Location)+ geom_vline(xintercept = 2019, color = "red", linetype =2)+
-  theme_bw()+ ylab("Total catch of Longfin Smelt per year")
+  theme_bw()+ ylab("Catch of Longfin Smelt per unit effort")
+
+
+ggplot(TR_z2z, aes(x = Year, y = loglong)) + geom_point()+ geom_line() +
+  facet_wrap(~Location)+ geom_vline(xintercept = 2019, color = "red", linetype =2)+
+  theme_bw()+ ylab("Catch of Longfin Smelt per unit effort (log-transformed)")
+
+
+
+#what about EDSM data from Grizzly Bay?
+EDSM = read_csv("data/raw/EDSM_KDTR.csv") %>%
+  filter(Subregion == "Grizzly Bay")
+
+#add zeros
+EDSM0s = mutate(EDSM, CPUE = Count/Volume) %>%
+  pivot_wider(id_cols = c(Subregion, StationCode, Latitude, Longitude, SampleDate, Volume),
+                     names_from = OrganismCode, values_fill = 0, values_from = CPUE, values_fn = sum) %>%
+  select(Subregion, StationCode, Latitude, Longitude, SampleDate, Volume, LFS) %>%
+  mutate(Year = year(SampleDate))
+
+ggplot(EDSM0s, aes(x = SampleDate, y =  LFS))+ geom_point()
+
+ggplot(EDSM0s, aes(x = as.factor(Year), y =  LFS))+ geom_boxplot()
+
+#mean CPUE
+
+EDSMmean = group_by(EDSM0s, Year) %>%
+  summarise(LFS = mean(LFS, na.rm =T)) %>%
+  mutate(Location = "EDSM Grizzly Bay", CPUE2 = LFS*10000, loglong = log(CPUE2+1))
+
+TR_z3 = bind_rows(TR_z2z, EDSMmean) %>%
+  mutate(Location = case_match(Location, "Grizzly Bay" ~ "FRP Grizzly Bay",
+                               "Ryer Island" ~ "FRP Ryer Island",
+                               "Tule Red" ~ "FRP Tule Red",
+                               .default = Location))
+
+ggplot(TR_z3, aes(x = Year, y =  loglong))+ geom_point()+ geom_line()+
+  geom_vline(xintercept = 2019, color = "red", linetype =2)+ facet_wrap(~Location, nrow =1)+
+  ylab("Mean catch of longfin smelt per 10000m3, \nlog-transformed")+ theme_bw()
+
+ggsave("plots/TuleRedLongfin.tiff", width =8, height =5)
+
+#export FRP data for record#export FRP datlocation()a for record
+Longfin2024_frp_forreport = read_csv("data/raw/2024.fish.FRP.EDI_2025-08-21.csv") %>%
+  filter(year(Date) == 2024, Location %in% c("Tule Red", "Grizzly Bay", "Ryer Island"), GearTypeAbbreviation != "HOOK") %>%
+  mutate(CPUE = Count/effort*10000, Year = year(Date)) %>%
+  pivot_wider(id_cols = c(SampleID_frp, Year,Location, Date,  LatitudeStart, LongitudeStart, effort, GearTypeAbbreviation),
+              names_from = CommonName, values_from = CPUE, values_fill = 0, values_fn = sum) %>%
+  select(SampleID_frp, LatitudeStart, LongitudeStart, GearTypeAbbreviation, effort, Location, Date, `Longfin Smelt`) %>%
+  rename(Longfin_CPUE = `Longfin Smelt`)
+
+write.csv(Longfin2024_frp_forreport, "data/Longfin_2024_frp.csv", row.names = F)
