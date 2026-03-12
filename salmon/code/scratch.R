80f11e687d9cb119bb89eb239961b5c08d45e48b
Pete-Nelson <pnelson1@ucsc.edu>
2026-03-12 11:01:53 -0700

    salmon modeling: acquire data, code and modeling

--- salmon/code/scratch.R
+++ salmon/code/scratch.R
@@ -1,6 +1,42 @@
 library(tidyverse)
 library(here)
 
+
+# seine data are in long format, including all fish spp...I want to retain that long
+# format, sum CHN counts for each seine--including 0's when no CHN are caught in
+# a given seine
+
+long <-
+  tibble(sample_id = c(1,1,1,2,2,3,3,3),
+         temp = c(20, 20, 20, 18, 18, 19, 19, 19),
+         organism_code = c("CHN", "CHN", "SACPIK", "SACPIK", "SACPIK", "WESMOS", "SACPIK", "CHN"),
+         count = c(2,1,1,1,3,6,1,1)) %>%
+  print()
+
+wider <-
+  long %>% pivot_wider(
+  # group by and retain 'temp'
+  id_cols = c(sample_id, temp),
+  # name new columns
+  names_from = organism_code,
+  # values from match
+  values_from = count,
+  # value if no match
+  values_fill = 0,
+  # add up multiple matches
+  values_fn = sum) %>%
+  print()
+
+# note that this 'long' version retains the counts (including 0's) for all spp in each sample_id
+long_again <-
+  wider %>%
+  pivot_longer(
+    cols = CHN:WESMOS,
+    names_to = "organism_code",
+    values_to = "count"
+  ) %>%
+  print()
+
 Petes_data <- here("~/Library/CloudStorage/OneDrive-CaliforniaDepartmentofWaterResources/3-Projects/09b-first flush/data files PN")
 
 # standardize function -----------
