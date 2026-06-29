e1fa0660068d3e67d8a3034aa37ae09e36bc0f0b
Pete-Nelson <pnelson1@ucsc.edu>
2026-06-29 11:38:13 -0700

    Stop tracking .DS_Store and other ignored files

--- salmon/code/site_wrangling.R
+++ salmon/code/site_wrangling.R
@@ -7,7 +7,7 @@ library(tidyverse)
 
 # load DJFMP station data & wrangle
 
-# first, region labels
+# region labels
 region_lookup <- tibble(
   region = factor(1:7),
   region_label = factor(c("Lower Sacramento River", "North Delta",
@@ -19,12 +19,10 @@ region_lookup <- tibble(
 
 # 1. create the base stations dataset from the DJFMP data (dt5 and dt3)
 stations <-
-  # stations lat/lon, etc
   read_rds(here("salmon/data/raw/dt5.rds")) %>%
   clean_names() %>%
   mutate(station = station_code) %>%
   tibble() %>%
-  # select stations used by beach seine program
   left_join(read_rds(here("salmon/data/raw/dt3.rds")) %>%
               clean_names() %>%
               mutate(region = region_code,
@@ -36,7 +34,6 @@ stations <-
               distinct(),
             by = "station",
             keep = F) %>%
-  # add region labels
   left_join(region_lookup, by = "region") %>%
   select(station = station_code, lon = longitude, lat = latitude, location = location.x,
          region, region_label, method_code)
@@ -45,13 +42,13 @@ stations <-
 stations_sf <- stations %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326)
 
-# 3. download Sacramento River flowlines using a bounding box from our stations
-# NOT using 3 lines below bc difficulties w USGS server; using saved flowline file
+# 3. download Sacramento River flowlines using a bounding box from your stations
 # bbox <- st_bbox(stations_sf)
 # nhd_data <- get_nhdplus(AOI = st_as_sfc(bbox), realization = "flowline")
 
 # load the flowlines and project to California Albers (meters) for accurate math
-# flowlines <- st_read(nhd_data, layer = "NHDFlowline", quiet = TRUE) %>% st_transform(crs = 3310)
+# flowlines <- st_read(nhd_data, layer = "NHDFlowline", quiet = TRUE) %>%
+  #st_transform(crs = 3310)
 
 stations_projected <- stations_sf %>%
   st_transform(crs = 3310)
@@ -63,17 +60,14 @@ flowlines <- read_rds(here("salmon/data/processed/flowlines")) %>%
 nearest_indexes <- st_nearest_feature(stations_projected, flowlines)
 
 # extract NHD flowline attributes (NHD provides upstream/downstream network distance properties)
-# 'pathlength' in NHD indicates kilometers to the basin outlet (defs for Sac R and SJR?!)
+# 'pathlength' in NHD indicates kilometers to the basin outlet
 stations_with_rkm <- stations %>%
   mutate(
     nhd_segment_index = nearest_indexes,
-    # pull the baseline distance-to-outlet value from the matched NHD streamline segment
-    river_km = flowlines$pathlength[nearest_indexes] # 'lengthkm' doesn't work!
+    # Pull the baseline distance-to-outlet value from the matched NHD streamline segment
+    river_km = flowlines$pathlength[nearest_indexes] # flowlines$Pathlength[nearest_indexes]
   ) %>%
   select(-nhd_segment_index)
 
-# check it out; note: these are ALL the beach seine stations
 print(stations_with_rkm)
-
-# save your work!
 write_rds(stations_with_rkm, here("salmon/data/processed/stations_with_rkm.rds"))
