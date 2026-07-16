471df6f1cf3636453baeabef8f73b9a93d13777a
Dave Bosworth <David.Bosworth@water.ca.gov>
2026-07-16 16:56:51 -0700

    Adding Sacramento River DWSC stations to the CWQ dataset

--- code/functions/cwq_resolve_station_merges.R
+++ code/functions/cwq_resolve_station_merges.R
@@ -20,6 +20,11 @@ resolve_station_merges <- function(df_data) {
     df_data <- merge_water_temp_sjr(df_data)
   }
 
+  # Merge RYI and RYF
+  if (exists("merge_ryi_and_ryf")) {
+    df_data <- merge_ryi_and_ryf(df_data)
+  }
+
   return(df_data)
 }
 
@@ -52,6 +57,24 @@ merge_water_temp_sjr <- function(df_data) {
     dplyr::arrange(date, factor(survey, levels = c("DWR-CEMP", "USGS"))) |>
     dplyr::slice(1, .by = date) |>
     dplyr::mutate(data_freq = "merged")
+
+  dplyr::bind_rows(df_composite, df_remaining)
+}
+
+# Merge USGS RYI and RYF 15-min data into one "station"
+merge_ryi_and_ryf <- function(df_data) {
+  target_stations <- c("RYI", "RYF")
+  df_target <- df_data |> dplyr::filter(station_abbr %in% target_stations)
+  df_remaining <- df_data |>
+    dplyr::filter_out(station_abbr %in% target_stations)
+
+  # Use RYI until 9/5/2018 - when USGS stopped collecting water quality at this station
+  df_composite <-
+    dplyr::bind_rows(
+      df_target |> dplyr::filter(station_abbr == "RYI" & date <= "2018-09-05"),
+      df_target |> dplyr::filter(station_abbr == "RYF" & date > "2018-09-05")
+    ) |>
+    dplyr::mutate(station_abbr = "RYI-RYF")
 
   dplyr::bind_rows(df_composite, df_remaining)
 }
